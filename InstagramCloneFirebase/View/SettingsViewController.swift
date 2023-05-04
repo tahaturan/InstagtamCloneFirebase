a7a3f85434344b7b92bc3ce4bbe98f1e4bd91678
tahaturan <tahaaturann16@gmail.com>
2023-05-04 15:03:08 +0300

    Upload ekrani tasaim & galeriden resim alma

--- InstagramCloneFirebase/View/SettingsViewController.swift
+++ InstagramCloneFirebase/View/SettingsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import Firebase
 
 class SettingsViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- InstagramCloneFirebase/View/UploadViewController.swift
+++ InstagramCloneFirebase/View/UploadViewController.swift
@@ -6,16 +6,49 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadViewController: UIViewController {
-
+    
+    @IBOutlet weak var selectImageView: UIImageView!
+    
+    @IBOutlet weak var commentTextField: UITextField!
+    
+    @IBOutlet weak var uploadButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        selectImageView.isUserInteractionEnabled = true
+        let gestureRocognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        selectImageView.addGestureRecognizer(gestureRocognizer)
     }
     
 
+    
 
+    @IBAction func uploadButton(_ sender: Any) {
+    }
+    
 
 }
+
+
+
+// MARK: Galeriden resim alma
+extension UploadViewController:UIImagePickerControllerDelegate , UINavigationControllerDelegate{
+    
+    @objc func chooseImage() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        selectImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+}
+
