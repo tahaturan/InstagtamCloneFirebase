62390ff47f317b2bd6abf43df180b56faf0a88fc
tahaturan <tahaaturann16@gmail.com>
2023-05-05 10:54:48 +0300

    verileri cekme

--- InstagramCloneFirebase/View/FeedViewController.swift
+++ InstagramCloneFirebase/View/FeedViewController.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController {
 
     @IBOutlet weak var tableview: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableview.delegate = self
         tableview.dataSource = self
+        getDataFromFirestore()
     }
     
 
@@ -27,18 +36,65 @@ class FeedViewController: UIViewController {
 extension FeedViewController: UITableViewDelegate , UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell" , for: indexPath) as! FeedTableViewCell
         
-        cell.commentLabel.text = "deneme"
-        cell.userNameLabel.text = "tahaaturan"
-        cell.likeLabel.text = "15"
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.userNameLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
         cell.userImageView.image = UIImage(named: "select.png")
         
         return cell
+    }
+    
+}
+
+
+// MARK: Firebase verileri cekme
+extension FeedViewController{
+    
+    func getDataFromFirestore()  {
+        
+        let fireStoreDatabase = Firestore.firestore()
+      
+        
+        fireStoreDatabase.collection("Posts").addSnapshotListener { snapshot, error in
+            
+            if error != nil{
+                print(error?.localizedDescription ?? "error")
+            }else{
+                if snapshot?.isEmpty != true && snapshot != nil{
+                    for document in  snapshot!.documents {
+                        let documentId = document.documentID
+                       
+                        
+                        if let userName = document.get("postedBy") as? String{
+                            self.userEmailArray.append(userName)
+                        }
+                        
+                        if let postComment = document.get("postComment") as? String{
+                            self.userCommentArray.append(postComment)
+                        }
+                        
+                        if let likes = document.get("likes") as? Int{
+                            self.likeArray.append(likes)
+                        }
+                        if let userImage = document.get("imageUrl") as? String{
+                            self.userImageArray.append(userImage)
+                        }
+                        
+                    }
+                    
+                    self.tableview.reloadData()
+                }
+                
+            }
+            
+        }
+        
     }
     
 }
